5fd987079b1af0c182e075900d877461f423af95
Victoria Ashworth <vashworth@google.com>
2024-03-14 16:00:02 -0500

    add tests

---
+++ dev/devicelab/bin/tasks/swift_package_manager_ios_test.dart
@@ -0,0 +1,27 @@
+// Copyright 2014 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:flutter_devicelab/framework/devices.dart';
+import 'package:flutter_devicelab/framework/framework.dart';
+import 'package:flutter_devicelab/framework/ios.dart';
+import 'package:flutter_devicelab/framework/task_result.dart';
+import 'package:flutter_devicelab/tasks/swift_package_manager_tests.dart';
+
+Future<void> main() async {
+  await task(() async {
+    deviceOperatingSystem = DeviceOperatingSystem.ios;
+    String? simulatorDeviceId;
+    try {
+      await testWithNewIOSSimulator('TestSwiftPackageManagerSim', (String deviceId) async {
+        simulatorDeviceId = deviceId;
+        await createSwiftPackageManagerTest(platform: 'ios', deviceIdOverride: deviceId)();
+      });
+    } finally {
+      print('remove');
+      await removeIOSSimulator(simulatorDeviceId);
+    }
+
+    return TaskResult.success(null);
+  });
+}
